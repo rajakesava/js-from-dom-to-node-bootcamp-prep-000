5d72860eed681bf40e670d66bb4928074733ce02
RAJA SOMALINGA K <rajakesava46@gmail.com>
2019-07-08 16:47:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // write your code below!
 console.log ("Hello,World!"); 
 console.log("Hello,World!");
-console.log("Hello,World!")
-console.log("Hello,World!")
+console.log("Hello,World!");
+console.log("Hello,World!");
 
 
